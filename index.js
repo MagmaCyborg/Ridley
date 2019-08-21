73efc32b777067062d951b9fa2c8995fed687129
MagmaCyborg <53702084+MagmaCyborg@users.noreply.github.com>
2019-08-21 16:20:50 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+const prefix = "=";
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ridley online!');
+})
+
+client.on('message', message => {
+    let args = message.content.substring(prefix.length).split(" ");
+
+    switch(args[0]) {
+        case 'help':
+            break;
+        case 'wiki':
+            if (!args[2]) {
+                const wiki = new Discord.RichEmbed()
+                .addField('https://enterthegungeon.gamepedia.com/' + args[1])
+                .setFooter('Wiki di Enter the gungeon');
+                message.channel.send(wiki);
+            } else {
+                const wiki = new Discord.RichEmbed()
+                .addField('https://enterthegungeon.gamepedia.com/' + args[1] + '_' + args[2])
+                .setFooter('Wiki di Enter the gungeon');
+                message.channel.send(wiki);
+            }
+            break;
+    }
+})
+
+client.login('NjAzMzYzMDUxNDkxODE5NTUw.XV1R8A.z6rgP6VWqiUwMe--Vunl9C8hO6A');
